04e06c3c3c4634b0415636da2ec5efeed00efc4d
Trond Graabræk <43748103+trondag@users.noreply.github.com>
2020-02-19 09:48:42 +0100

    Add project files.

---
+++ ConsoleApp2/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using WebRender4;
+
+namespace ConsoleApp2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Website.hei(args);
+        }
+    }
+}
